e6cc9d25fc17662224dcb7a7759ab6a8de32d7ed
sbe04146 <sbe04146@naver.com>
2021-09-27 11:24:48 +0900

    김유성

---
+++ src/main/java/com/spring/bank/user/vo/AccountVO.java
@@ -0,0 +1,96 @@
+package com.spring.bank.user.vo;
+
+import java.sql.Date;
+
+public class AccountVO {
+
+	private String account_id;
+	private int account_password;
+	private String member_id;
+	private int account_balance;
+	private int account_type;
+	private int account_state;
+	private String account_stateContent;
+	private int account_limit;
+	private Date account_deleteDate;
+	private Date account_sleepDate;
+	private Date account_newDate;
+	private int account_bankCode;
+	
+	
+	public String getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(String account_id) {
+		this.account_id = account_id;
+	}
+	public int getAccount_password() {
+		return account_password;
+	}
+	public void setAccount_password(int account_password) {
+		this.account_password = account_password;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public int getAccount_balance() {
+		return account_balance;
+	}
+	public void setAccount_balance(int account_balance) {
+		this.account_balance = account_balance;
+	}
+	public int getAccount_type() {
+		return account_type;
+	}
+	public void setAccount_type(int account_type) {
+		this.account_type = account_type;
+	}
+	public int getAccount_state() {
+		return account_state;
+	}
+	public void setAccount_state(int account_state) {
+		this.account_state = account_state;
+	}
+	public String getAccount_stateContent() {
+		return account_stateContent;
+	}
+	public void setAccount_stateContent(String account_stateContent) {
+		this.account_stateContent = account_stateContent;
+	}
+	public int getAccount_limit() {
+		return account_limit;
+	}
+	public void setAccount_limit(int account_limit) {
+		this.account_limit = account_limit;
+	}
+	public Date getAccount_deleteDate() {
+		return account_deleteDate;
+	}
+	public void setAccount_deleteDate(Date account_deleteDate) {
+		this.account_deleteDate = account_deleteDate;
+	}
+	public Date getAccount_sleepDate() {
+		return account_sleepDate;
+	}
+	public void setAccount_sleepDate(Date account_sleepDate) {
+		this.account_sleepDate = account_sleepDate;
+	}
+	public Date getAccount_newDate() {
+		return account_newDate;
+	}
+	public void setAccount_newDate(Date account_newDate) {
+		this.account_newDate = account_newDate;
+	}
+	public int getAccount_bankCode() {
+		return account_bankCode;
+	}
+	public void setAccount_bankCode(int account_bankCode) {
+		this.account_bankCode = account_bankCode;
+	}
+
+	
+
+}
---
+++ src/main/webapp/resources/js/transfer_page.js
@@ -0,0 +1,77 @@
+// 계좌 비밀번호 확인
+function account_confirmPwd(){
+	if(!document.transferform.account_password.value){
+		alert("계좌 비밀번호를 입력하세요");
+		document.transferform.account_password.focus();
+		return false;
+	}
+	
+	var url = "account_confirm?account_password=" + document.transferform.account_password.value;
+	window.open(url, "confirm", "menubar=no, width=500, height=400");
+}
+
+function account_confirmPwdFocus(){
+	document.confirmform.account_password.focus();
+}
+
+function confirmPwdCheck(){
+	if(!document.confirmform.account_password.value){
+		alert("계좌 비밀번호를 입력하세요");
+		document.confirmform.account_password.focus();
+		return false;
+	}
+}
+
+function setId(account_password) {
+	opener.document.transferform.account_password.value = account_password; 		// opener = window 객체의 open()
+	opener.document.transferform.hiddenPassword.value = 1;	// 메서드로 열린 새 창(=중복확인창)에서
+	self.close();									// 부모창(=회원가입)에 접근할 때 사용
+}
+
+
+// 이체시 필요한 정보 입력
+function transferCheck() {
+
+	// 계좌 비밀번호
+	if (!document.transferform.account_password.value) {
+		alert("계좌 비밀번호를 입력하세요");
+		document.transferform.account_password.focus();
+		return false;
+
+		// 입금계좌번호
+	} else if (!document.transferform.transfer_senderAccount.value) {
+		alert("입금할 계좌번호를 입력하세요");
+		document.transferform.transfer_senderAccount.focus();
+		return false;
+
+		// 이체 금액
+	} else if (!document.transferform.transfer_money.value) {
+		alert("입금할 금액을 입력하세요");
+		document.transferform.transfer_money.focus();
+		return false;
+	}
+	return true;
+}
+
+// 이체 금액 버튼 누를시 해당 금액 추가
+function addPrice() {
+	const resultElement = document.getElementById('transfer_money');
+
+	let number = resultElement.innerText;
+
+	if (document.transferform.transfer_money.value === '10000') {
+		number = parseInt(number) + 10000;
+	} else if (document.transferform.transfer_money.value === '30000') {
+		number = parseInt(number) + 30000;
+	} else if (document.transferform.transfer_money.value === '50000') {
+		number = parseInt(number) + 50000;
+	} else if (document.transferform.transfer_money.value === '100000') {
+		number = parseInt(number) + 100000;
+	} else if (document.transferform.transfer_money.value === '500000') {
+		number = parseInt(number) + 500000;
+	} else if (document.transferform.transfer_money.value === '1000000') {
+		number = parseInt(number) + 1000000;
+	}
+
+	resultElement.innerText = number;
+}
